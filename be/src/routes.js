0b88ef73edd2971d06c8ef19ddb63dfe2ccc6389
Erlliam Mejia <tommejas@gmail.com>
2025-05-01 14:02:15 -0500

    Wip tracking almost done

--- be/src/routes.js
+++ be/src/routes.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export default function (fastify, options) {
   fastify.get("/tracking", async (request, reply) => {
     try {
@@ -25,12 +27,37 @@ export default function (fastify, options) {
   };
   fastify.post("/tracking", trackingOptions, async (request, reply) => {
     try {
-      await fastify.pg.query(`
+      const { name } = request.body;
+      if (!name) {
+        return reply.code(400).send("Error: Name must be provided");
+      }
+
+      const userResponse = await axios.get(
+        "https://api.twitch.tv/helix/users",
+        {
+          headers: {
+            Authorization: "Bearer 4u1v9u8lc6apa5u2u94ftg554mqq24",
+            "Client-Id": "f7cf6pk6ez8ccdeieryzkunmdewymq",
+          },
+          params: {
+            login: name,
+          },
+        }
+      );
+
+      const userId = userResponse.data.data[0].id;
+      // todo insert into my users table for the foreign key stuff
+
+      const text = `
         INSERT INTO track (channel_id)
-        VALUES ($1)`);
+        VALUES ($1)`;
+      const values = [userId];
+      await fastify.pg.query(text, values);
     } catch (error) {
       console.log(error);
-      return "Error: Failed to add channel to tracking table";
+      return reply
+        .code(500)
+        .send("Error: Failed to add channel to tracking table");
     }
   });
 }
